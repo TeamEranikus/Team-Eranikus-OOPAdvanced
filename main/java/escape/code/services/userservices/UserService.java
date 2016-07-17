d53a23594842b3caab472cb969fc5e99a3900ada
elitasheva <elica.tasheva@gmail.com>
2016-07-17 17:14:03 +0300

    clean up

--- main/java/escape/code/services/userservices/UserService.java
+++
@@ -1,9 +0,0 @@
-package escape.code.services.userservices;
-
-import escape.code.models.User;
-
-public interface UserService {
-    void createUser(String username, String password);
-    void updateUser(User user);
-    User getUser(String username, String password);
-}
